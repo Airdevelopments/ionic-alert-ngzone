33c9d2f469fdd2ea82361cbb1e5919853b12854c
Mikis Wöhrmann <m.woehrmann@hs-solutions.de>
2024-02-19 14:51:32 +0100

    ngzone issue example

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, NgZone } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController,
+  ) {}
+
+  async openAlert() {
+    const alert = await this.alertController.create({
+      header: 'My Alert',
+      buttons: [
+        {
+          text: 'Click me',
+          handler: async () => {
+            const toast = await this.toastController.create({ header: `Is running inside angular zone: ${NgZone.isInAngularZone()}`, duration: 5000, position: 'top' });
+            await toast.present();
+            NgZone.assertInAngularZone();
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
 }
